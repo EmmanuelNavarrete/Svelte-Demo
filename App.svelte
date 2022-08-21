82fcf89159f43ca0ddd8b711c9a3be4fa3e5e765
Emmanuel <86861595+EmmanuelNavarrete@users.noreply.github.com>
2022-08-21 17:40:21 -0500

    Initial commit

---
+++ App.svelte
@@ -0,0 +1,10 @@
+<script>
+import Form from "./form.svelte";
+</script>
+
+
+
+<main>
+  <h2>Peliculas</h2>
+  <Form/>
+</main>
---
+++ form.svelte
@@ -0,0 +1,28 @@
+<script>
+  const movies = [
+    {
+      name: "Terminator",
+      boletos: 0,
+      available: 5
+    },
+    {
+      name: "La catrina",
+      boletos: 0,
+      available: 2
+    }
+  ];
+</script>
+
+{#each movies as movie}
+
+<form>
+	<h3>{movie.name}</h3>
+	<button type="button" disabled={movie.boletos <= 0} on:click = {()=> movie.boletos -=1}>
+  -
+  </button>
+	{movie.boletos}
+	<button type="button" disabled={movie.boletos >= movie.available} on:click = {()=> movie.boletos +=1}>
+  +
+  </button>
+</form>
+{/each}
